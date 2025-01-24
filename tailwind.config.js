5736a988b829232235dc8c5a92a7ae1e81932038
Timothy Kompanchenko <tymofi@mac.com>
2025-01-24 11:38:50 -0500

    Bootstrap the website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
+
+      extend: {
+        aspectRatio: {
+          '1108/632': 1108 / 632,
+        },
+        backgroundImage: {
+          'linear-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
+        },
+      },
+  
+
+    },
+  },
+};

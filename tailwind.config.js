ebcab98294b9df84d77051ac9e39975cd008cfca
Timothy Kompanchenko <tymofi@mac.com>
2025-01-24 13:32:11 -0500

    Website v1 candidate

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
         sans: ["var(--font-geist-sans)"],
         mono: ["var(--font-geist-mono)"],
       },
-
-      extend: {
-        aspectRatio: {
-          '1108/632': 1108 / 632,
-        },
-        backgroundImage: {
-          'linear-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
-        },
-      },
-  
-
     },
   },
 };
